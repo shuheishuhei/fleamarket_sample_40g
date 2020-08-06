012f60191ab9d7fd5f5293c6242d95b190121141
ympon <tyasson0u0@gmail.com>
2020-08-06 17:45:44 +0900

    add image and fix classname

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  # before_action :basic_auth
 
-  private
+  # private
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials[:basic_auth][:user] &&
-      password == Rails.application.credentials[:basic_auth][:pass]
-    end
-  end
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == Rails.application.credentials[:basic_auth][:user] &&
+  #     password == Rails.application.credentials[:basic_auth][:pass]
+  #   end
+  # end
 
-  def production?
-    Rails.env.production?
-  end
+  # def production?
+  #   Rails.env.production?
+  # end
 end
