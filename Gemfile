cc96f942824f8af8a4f61a2bb4ac0b4a54ebbde5
hiromimatsu <hiro.ay12@icloud.com>
2020-08-08 15:21:17 +0900

    test-code

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ group :development, :test do
   gem 'capistrano3-unicorn'
   gem 'capistrano-rails-console'
   gem 'rspec-rails', '~> 4.0.0.beta2'
+  gem 'factory_bot_rails'
 end
 
 group :development do
@@ -65,3 +66,4 @@ group :production do
 end
 
 gem 'devise'
+gem "haml-rails", ">= 1.0", '<= 2.0.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  devise_scope :user do
+    get 'profiles', to: 'users/registrations#new_profile'
+    post 'profiles', to:'users/registrations#create_profile'
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
   end
   root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
