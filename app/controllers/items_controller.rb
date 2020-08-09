15bba068de467db62f32268ea36bb6486ecf001d
ympon <tyasson0u0@gmail.com>
2020-08-09 16:30:14 +0900

    save items & images

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,17 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    @item.images.new
   end
 
   def create
+    @item =Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -17,4 +25,10 @@ class ItemsController < ApplicationController
 
   def destroy
   end
+end
+
+private
+
+def item_params
+  params.require(:item).permit(:name, :price, images_attributes: [:src])
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   has_many :images
   mount_uploader :image, ImageUploader
+  accepts_neated_attributes_for :images, allow_destroy: true
 end
