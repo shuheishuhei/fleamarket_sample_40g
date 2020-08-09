cf7b9bb2324894faf753e90dbc0d0c5fca6979ca
ympon <tyasson0u0@gmail.com>
2020-08-09 16:59:47 +0900

    add update function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, except: [:index, :new, :create]
+
   def index
     @items = Item.includes(:images).order('created_at DESC')
   end
@@ -21,6 +23,11 @@ class ItemsController < ApplicationController
   end
 
   def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -30,5 +37,9 @@ end
 private
 
 def item_params
-  params.require(:item).permit(:name, :price, images_attributes: [:src])
+  params.require(:item).permit(:name, :price, images_attributes: [:src, :_destroy, :id])
+end
+
+def set_item
+  @item = Item.find(params[:id])
 end
