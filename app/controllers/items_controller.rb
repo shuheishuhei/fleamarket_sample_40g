77396ff6f450ed7f00e949f7774cc6cd4dfa5ccc
ympon <tyasson0u0@gmail.com>
2020-08-10 14:45:50 +0900

    add post function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,22 +12,14 @@ class ItemsController < ApplicationController
 
   def create
     @item =Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
+    @item.save
   end
 
   def edit
   end
 
   def update
-    if @item.update(item_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
+    @item.update(item_params)     
   end
 
   def destroy
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,4 @@
 class Image < ApplicationRecord
   mount_uploadder :src, ImageUploader
+  belongs_to :image
 end
