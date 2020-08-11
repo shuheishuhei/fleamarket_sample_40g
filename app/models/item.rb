505319a1bdc68cf12da3bf22a1ea03d84822c9f3
shuheishuhei <shuhei.2215@gmail.com>
2020-08-11 12:22:14 +0900

    出品ページにactive_hash導入した

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :burden
+  belongs_to_active_hash :day
 end
