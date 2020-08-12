51ca91f6a28783fa026d8b2e4d267d6819ddc7f2
ympon <tyasson0u0@gmail.com>
2020-08-12 20:10:49 +0900

    preview function

--- app/javascript/items.js
+++ app/javascript/items.js
@@ -9,16 +9,41 @@ $(document).on("turbolinks:load", ()=> {
     return html;
   }
 
+  const buildImg = (index, url)=> {
+    const html = `<img data-index="${index}" image="${url}" width="100px" height="100px">`;
+    return html;
+  }
+
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
 
+  lastIndex = $('.js-file_group:last').data('index');
+  fileIndex.splice(0, lastIndex);
+
+  $('.hidden-destroy').hide();
+
   $('#image-box').on('change', '.js-file', function(e) {
-    $('#image-box').append(buildFileField(fileIndex[0]));
-    fileIndex.shift();
-    fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+    const targetIndex = $(this).parent().data('index');
+    const file = e.target.files[0];
+    const blobUrl = window.URL.createObjectURL(file);
+    if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
+      img.setAttribute('image', blobUrl);
+    } else {
+      $('#previews').append(buildImg(targetIndex, blobUrl));
+
+      $('#image-box').append(buildFileField(fileIndex[0]));
+      fileIndex.shift();
+      fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+    }
   });
 
   $('#image-box').on('click', '.js-remove', function() {
+    const targetIndex = $(this).parent().data('index')
+    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+    if (hiddenCheck) hiddenCheck.prop('checked', true);
+
     $(this).parent().remove();
+    $(`img[data-index="${targetIndex}"]`).remove();
+    
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
 });
