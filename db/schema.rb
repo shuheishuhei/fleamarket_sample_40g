bcc6c0fd95905f84e756d2c4fb1c6952ae731877
tottiman <toshiki.mmtk@gmail.com>
2020-08-13 15:41:32 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_001502) do
+ActiveRecord::Schema.define(version: 2020_08_09_220702) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "destination_first_name", null: false
@@ -65,7 +65,6 @@ ActiveRecord::Schema.define(version: 2020_08_13_001502) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
