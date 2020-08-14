6955076625817ecddfa4270e483a8dd1d1554d09
hiromimatsu <hiro.ay12@icloud.com>
2020-08-14 21:43:24 +0900

    ビュー崩れの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,7 +21,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path, notice: '出品完了しました'
     else
