58e47bf2f6f5e660006f2551fa24d84020a8afd9
shuheishuhei <shuhei.2215@gmail.com>
2020-08-18 18:44:04 +0900

    test

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
   validates :category_id, presence: true
   validates :status_id, presence: true
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
+  extend ActiveHash::Associations::ActiveRecordExtensions 
 
   has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images, allow_destroy: true
