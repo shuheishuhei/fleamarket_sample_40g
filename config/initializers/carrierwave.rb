31ee96a39dbc3ef7002373d0e5efebcc130d4690
shuheishuhei <shuhei.2215@gmail.com>
2020-08-18 20:39:24 +0900

    S3の準備

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,7 +12,7 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-norteast-1'
+      region: 'ap-northeast-1'
     }
     config.fog_directory = 'fleamarketsample40g'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fleamarketsample40g'
