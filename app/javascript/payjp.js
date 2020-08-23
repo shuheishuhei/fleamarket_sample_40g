3148184bd1db05851c0a2e80aac18aa90fd3bba8
tottiman <toshiki.mmtk@gmail.com>
2020-08-23 21:08:02 +0900

    微調整

--- app/javascript/payjp.js
+++ app/javascript/payjp.js
@@ -3,10 +3,10 @@ window.addEventListener('turbolinks:load', function(){
   //id名が"payment_card_submit-button"というボタンが押されたら取得
   let submit = document.getElementById("payment_card_submit-button");
   
+
   Payjp.setPublicKey('pk_test_4352092f8deb95210dde9e0e'); //公開鍵
-
     submit.addEventListener('click', function(e){ //ボタンが押されたらトークン作成
-
+    
     e.preventDefault(); //ボタンを1度無効化
       
     let card = { //入力されたカード情報を取得
--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :card do
-    user_id { 1 }
     customer_id { 0 }
     card_id { 0 }
+    user_id { 1 }
   end
 end
