400fe9de38fc6fe354aef3ae501ffff315242897
tottiman <toshiki.mmtk@gmail.com>
2020-08-24 13:16:28 +0900

    update

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :card do
-    customer_id { 0 }
-    card_id { 0 }
-    user_id { 1 }
+    customer_id {0}
+    card_id {0}
+    user_id {2}
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
   describe Card do
     describe '#create' do
       it "クレジットカードは登録できます" do
-      card = build(:card)
-      expect(card).to be_valid
+        card = build(:card)
+        expect(card).to be_valid
       end
       it "カード番号なしでは登録できません" do
-      card = build(:card, customer_id: "")
-      card.valid?
-      expect(card.errors[:customer_id]).to include("を入力してください")
+        card = build(:card, customer_id: "")
+        card.valid?
+        expect(card.errors[:customer_id]).to include("を入力してください")
       end
       it "有効期限なしでは登録できません" do
-      card = build(:card, card_id: "")
-      card.valid?
-      expect(card.errors[:card_id]).to include("を入力してください")
+        card = build(:card, card_id: "")
+        card.valid?
+        expect(card.errors[:card_id]).to include("を入力してください")
       end
+    end
   end
-end
 
