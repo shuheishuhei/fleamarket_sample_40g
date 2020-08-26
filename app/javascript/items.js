cd6e1f3f25a3209821a685536801279828c8df5e
tottiman <toshiki.mmtk@gmail.com>
2020-08-26 09:40:20 +0900

    不要

--- app/javascript/items.js
+++ app/javascript/items.js
@@ -1,4 +1,12 @@
 $(document).on("turbolinks:load", ()=> {
+  const buildImg = (index, url)=> {
+    const html = `<div class="image__post__area" data-index="${index}">
+                    <img data-index="${index}" src="${url}" width="100%" height="80%">
+                    <div class="js-remove">削除</div>
+                  </div>`;
+    return html;
+  }
+
   const buildFileField = (num)=> {
     const html = `<div data-index="${num}" class="js-file_group">
                     <input class="js-file" type="file"
@@ -8,24 +16,13 @@ $(document).on("turbolinks:load", ()=> {
     return html;
   }
 
-  function changeLabelFor(num) {
-    $(".image__block__area__label").attr("for", `item_images_attributes_${num}_image`)
-  }
-
-  const buildImg = (index, url)=> {
-    const html = `<div class="image__post__area" data-index="${index}">
-                    <img data-index="${index}" src="${url}" width="100%" height="80%">
-                    <div class="js-remove">削除</div>
-                  </div>`;
-  
-    return html;
-  }
+  // function changeLabelFor(num) {
+  //   $(".image__block__area__label").attr("for", `item_images_attributes_${num}_image`)
+  // }
 
   let fileIndex = [1,2,3,4,5];
-
   lastIndex = $(".js-file_group:last").data("index");
   fileIndex.splice(0, lastIndex);
-
   $(".hidden-destroy").hide();
 
   $("#image-box").on("change", ".js-file", function(e) {
@@ -36,39 +33,40 @@ $(document).on("turbolinks:load", ()=> {
       img.setAttribute("src", blobUrl);
     } else {
       $("#previews").append(buildImg(targetIndex, blobUrl));
-
       $("#image-box").append(buildFileField(fileIndex[0]));  
-        
-        // ５枚以上投稿しようとしたら投稿範囲がなくなる
-        if ($(".image__post__area").length >= 5){  
-          $(".image__block__area").hide();
-        }
-        
-      
-      let num = $(`.js-file_group`).last().data("index")
-      changeLabelFor(num)
       fileIndex.shift();
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+        
+        // ５枚以上投稿しようとしたら投稿範囲がなくなる
+        // if ($(".image__post__area").length >= 5){  
+        //   $(".image__block__area").hide();
+        // }
+        
+      
+      // let num = $(`.js-file_group`).last().data("index")
+      // changeLabelFor(num)
+      
     }
     
   });
 
 
   $("#image-box").on("click", ".js-remove", function() {
+    $(this).parent().remove();
     const targetIndex = $(this).parent().data("index");
     const hiddenCheck = $(`#item_item_images_attributes_${targetIndex}__destroy`)[0];
-    if ($(hiddenCheck).prop("checked", true));
-
-    $(this).parent().remove();
-    $(`#item_images_attributes_${targetIndex}_image`).remove();
-    $(`img[data-index="${targetIndex}"]`).remove();
-
-    if ($(".image__post__area").length < 5){  
-      $(".image__block__area").show();
-    }
-
+    if (hiddenCheck) hiddenCheck.prop("checked", true));
     if ($(".js-file").length == 0) $("#image-box").append(buildFileField(fileIndex[0]));
+    $(`img[data-index = ${targetIndex}]`).remove();
+   
+    // $(`#item_images_attributes_${targetIndex}_image`).remove();
+    
 
+    // if ($(".image__post__area").length < 5){  
+    //   $(".image__block__area").show();
+    // }
+
+    
   });
 
   if ($(".image__post__area").length >= 5){  
